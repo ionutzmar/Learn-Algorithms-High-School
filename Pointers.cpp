b01a7b61b8f470b81ecfd56e58eded7ee794da74
Ionut Marius Mihalache <ionutzmar@gmail.com>
2015-10-18 18:50:01 +0300

    learn using (int *)

--- Pointers.cpp
+++ Pointers.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	int *p, n;
-	n = 5;
-	p = &n;
-	cout << "&n: " << &n << "\np: " << p << "\n*p: " << *p << "\n&p: " << &p << endl;
-	cout << "Size of int: " << sizeof(int);
-	int *a, b;
-	b = 5;
-	a = &b;
-	cout << "\n&b: " << &b << "\na: " << a << "\n*a: " << *a << "\n&a: " << &a << endl;
-	int q = 7, *adr = &q;
-	cout << *adr;
+	int *a, c;
+	float *d, e;
+	a = &c;
+	d = &e;
+	e = 2.5;
+
+
+	cout << "a: " << a << "\nd: " << d << endl;
+	a = (int *)d;
+	cout << "a: " << a << "\nd: " << d << endl;
+	cout << "*a: " << *a << "\n*d: " << *d << endl;
+	cout << "size of float: " << sizeof(float);
 	return 0;
 }
