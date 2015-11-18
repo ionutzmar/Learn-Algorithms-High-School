799f37d8d2c6cffc6bcc5bfd6411fdd6957ac47f
Ionut Marius Mihalache <ionutzmar@gmail.com>
2015-11-18 18:43:22 +0200

    Add nodes to list

--- list.cpp
+++ list.cpp
@@ -10,8 +10,65 @@ struct node
 };
 
 node *v;
+int counter = 1;
 
-node* addNode()
+void addNodeLast(node* v,int value)  //add a node with info = value at the end of the list v
+{
+	if (v -> next_adr)
+	{
+		addNodeLast(v -> next_adr, value);
+	}
+	else
+	{
+		node* c = new node;
+		v -> next_adr = c;
+		c -> next_adr = 0;
+		c -> info = value;
+	}
+}
+void addNode(node* l, int value, int p)  //and a node to the list v at the position p. If p == -1 the node will be added at the end of the list
+{
+	if (p == 0)
+	{
+		node* c = new node;
+		c -> info = value;
+		c -> next_adr = l;
+		v = c;
+
+	}
+	else if (p > 0)
+	{
+		if(counter == p)
+		{
+			node* c = new node;
+			c -> info = value;
+			c -> next_adr = l -> next_adr;
+			l -> next_adr = c;
+			counter = 1;
+			return;
+		}
+		else
+		{
+			counter++;
+			if (l -> next_adr)
+				addNode(l -> next_adr, value, p);
+			else
+				counter = 1;
+			return;
+		}
+	}
+	else if(p == -1)
+	{
+		addNodeLast(v, value);
+	}
+}
+
+void inverseList(node* v)  //inverses the list v
+{
+
+}
+
+node* createList()
 {
 	node* c;
 	int nr;
@@ -19,7 +76,7 @@ node* addNode()
 	if(nr)
 	{
 		c = new node;
-		c -> next_adr = addNode();
+		c -> next_adr = createList();
 		c -> info = nr;
 		return c;
 	}
@@ -27,7 +84,7 @@ node* addNode()
 		return 0;
 }
 
-void show(node* v)
+void show(node* v)  //It shows the list in the right order.
 {
 	node *c = v;
 	while(c)
@@ -37,9 +94,29 @@ void show(node* v)
 	}
 }
 
+void showInv(node* v) //It shows the list in the invers order without the first element of the list.
+{
+	if(v -> next_adr)
+	{
+		v = v -> next_adr;
+		showInv(v);
+		cout << v-> info << endl;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	v = addNode();
+	v = createList();
 	show(v);
+
+	int pos, val;
+	while(1)
+	{
+		cin >> val >> pos;
+		cout << "\n";
+		addNode(v, val, pos);
+		show(v);
+	}
+	
 	return 0;
 }
